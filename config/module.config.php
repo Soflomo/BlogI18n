680c58226c6dc83c1eac5378e94c53fdee4f5e2b
Jurian Sluiman <jurian@soflomo.com>
2013-08-21 16:56:08 +0200

    Initial commit

---
+++ config/module.config.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+    'soflomo_blog' => array(
+        'article_entity_class' => 'Soflomo\BlogI18n\Entity\Article',
+    ),
+
+    'doctrine' => array(
+        'driver' => array(
+            'soflomo_blog_i18n' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/mapping'
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+                    'Soflomo\BlogI18n\Entity' => 'soflomo_blog_i18n',
+                ),
+            ),
+        ),
+        'entity_resolver' => array(
+            'orm_default' => array(
+                'resolvers' => array(
+                    'Soflomo\Blog\Entity\ArticleInterface' => 'Soflomo\BlogI18n\Entity\Article',
+                ),
+            ),
+        ),
+    ),
+);
